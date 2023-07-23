79bde240c0769f6ef3e7b5c30a7831e0726e31c6
Mohamed BC <85040912+Mohamed-Bencheikh@users.noreply.github.com>
2023-07-23 19:22:58 +0100

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,34 +1,40 @@
 import requests
 import streamlit as st
+from PIL import Image
+import time
 
 api_key = st.secrets['OPEN_WEATHER_API']
 
 
 def getWeather(location):
-    url = f"http://api.openweathermap.org/res/2.5/weather?q={location}&units=metric&appid={api_key}"
-    response = requests.get(url)
-    print(response.status_code)
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&units=metric&appid={api_key}"
+    response = requests.get(url).json()
+    return response
 
 
-getWeather("Casablanca")
-# print(api_key)
-
-# st.title('Welcome to :blue[NICY]')
-# st.subheader('Your Weather App')
-# loc = st.text_input(label='location', placeholder='search for location')
-# if st.button('show', type='primary') and loc != '':
-#     res = getWeather(loc)
-#     data = {}
-#     data['country'] = res['sys']['country']
-#     data['city'] = res['sys']['name']
-#     data['lon'] = res['coord']['lon']
-#     data['lat'] = res['coord']['lat']
-#     data['desc'] = res['weather'][0]['description']
-#     data['icon'] = res['weather'][0]['icon']
-#     data['curr temp'] = res['main']['temp']
-#     data['temp min'] = res['main']['temp_min']
-#     data['temp max'] = res['main']['temp_max']
-#     data['pressure'] = res['main']['pressure']
-#     data['humidity'] = res['main']['humidity']
-#     data['wind speed'] = res['wind']['speed']
-#     st.dataframe(data)
+st.title('Welcome to :blue[NICY]')
+st.subheader('Your Weather App')
+loc = st.text_input(label='location', placeholder='search for location')
+if st.button('show', type='primary') and loc != '':
+    with st.spinner('getting '+loc+' weather ...'):
+        time.sleep(3)
+    res = getWeather(loc)
+    data = {}
+    data['country'] = res['sys']['country']
+    data['city'] = res['name']
+    data['lon'] = res['coord']['lon']
+    data['lat'] = res['coord']['lat']
+    data['desc'] = res['weather'][0]['description']
+    data['curr temp'] = res['main']['temp']
+    data['temp min'] = res['main']['temp_min']
+    data['temp max'] = res['main']['temp_max']
+    data['pressure'] = res['main']['pressure']
+    data['humidity'] = res['main']['humidity']
+    data['wind speed'] = res['wind']['speed']
+    c1, c2 = st.columns(2)
+    with c1:
+        st.dataframe(data, width=300)
+    with c2:
+        icon = res['weather'][0]['icon']
+        img = Image.open('./icons/'+icon+'.png')
+        st.image(image=img)
